4d2562b7a086076b0740fe9f329a8eec1e17b19b
p0vned <strzebonsky@gmail.com>
2021-03-12 19:02:02 +0100

    Added runge kutta solver interface

---
+++ App/Interfaces/IRungeKuttaSolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Interfaces
+{
+    interface IRungeKuttaSolver
+    {
+        int[] aCoefficients { get; }
+        int[] bCoefficients { get; }
+        int[,] cCoeficcients { get; }
+
+        double Solve(double xi, double yi);
+    }
+}
