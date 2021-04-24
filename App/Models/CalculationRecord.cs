84d2cdb5cb18e39d7eb3696df1f21ac9e59b3e64
p0vned <strzebonsky@gmail.com>
2021-04-24 17:00:09 +0200

    Changed column names in the exported CSV file

--- App/Models/CalculationRecord.cs
+++ App/Models/CalculationRecord.cs
@@ -1,10 +1,20 @@
-﻿namespace App.Models
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace App.Models
+
 {
     class CalculationRecord
     {
+        [Name("Calculation point value")]
         public double XValue { get; set; }
+
+        [Name("Calculated value")]
         public double SolverCalculationResult { get; set; }
+
+        [Name("Exact solution value")]
         public double ExactSolutionResult { get; set; }
+
+        [Name("Error")]
         public double Error { get; set; }
 
         public CalculationRecord(double xValue, double solverCalculationResult, double exactSolutionResult, double error)
